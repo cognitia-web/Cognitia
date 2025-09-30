4dd0945c7cc2aab8afaa7f9d03b8b8b9b9b70bfc
cognitia-web <cognitia.yash@gmail.com>
2025-09-30 22:05:48 +0530

    del

--- server/db.ts
+++
@@ -1,5 +0,0 @@
-// Database connection using Firebase Firestore
-import { firestoreService } from './services/firestore';
-
-// Export the Firestore service for use throughout the application
-export const db = firestoreService;
